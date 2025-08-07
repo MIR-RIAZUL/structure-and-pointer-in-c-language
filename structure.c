e5d345b331bf9bb062fbd2a4cdc366072094a422
MIR-RIAZUL <mirriazul859@gmail.com>
2025-08-08 02:44:24 +0600

    add  name array

--- structure.c
+++ structure.c
@@ -2,6 +2,7 @@
 
 //global structures
 struct person {
+    char name[50];
     int age;
     float salary;
 
@@ -13,14 +14,14 @@ int main() {
     struct person person[4];
     for(int i=0;i<4;i++)
 {
-    printf("enter age and salary of person %d: ", i+1);
-    scanf("%d %f", &person[i].age, &person[i].salary);
+    printf("enter name age and salary of person %d: ", i+1);
+    scanf("%s %d %f", person[i].name, &person[i].age, &person[i].salary);
 
 }
     printf("\n\nDetails of persons:\n");
     for(int i=0;i<4;i++)
     {
-        printf("Person %d: Age = %d, Salary = %.2f\n", i+1, person[i].age, person[i].salary);
+        printf("Person %d:\n Name = %s, Age = %d, Salary = %.2f\n", i+1, person[i].name, person[i].age, person[i].salary);
     }
 
     return 0;
